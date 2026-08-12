e6188bf16c4f3f045216f820a1b616c8d5bf51fc
Ceisar Ardi Purnomo Saputra Efendi Nasution <163375859+Jackllyyn@users.noreply.github.com>
2026-08-12 22:23:09 +0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -49,7 +49,8 @@ const db = mysql.createPool({
     host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_NAME || 'db_ahp',
+    database: process.env.DB_NAME || 'railway',
+    port: process.env.DB_PORT || 3306,  
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
